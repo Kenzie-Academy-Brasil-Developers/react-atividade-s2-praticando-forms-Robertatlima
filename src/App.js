42abab443379e74d1e8f67cf6bca4ede6acead61
Roberta Lima <robertataisaa@gmail.com>
2021-09-14 17:30:31 -0300

    Atividade finalizada

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import "./App.css";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import CardUser from "./components/cardUser";
+import { useState } from "react";
+
+function App() {
+  const [card, setCard] = useState("");
+
+  const formSchema = yup.object().shape({
+    user: yup.string().required("Campo obrigatório"),
+    name: yup
+      .string()
+      .required("Campo obrigatório")
+      .matches("^[A-Za-zÀ-ü ]{0,18}$", "Nome deve ter no maximo 18 caracteres"),
+    email: yup.string().required("Campo obrigatório").email("email invalido"),
+    cellphone: yup.string().required("Campo obrigatório"),
+    password: yup.string().required("Campo obrigatório"),
+    // .matches(
+    //   "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$",
+    //   "essa senha não é segura")
+    passwordConfirmed: yup.string().required("Campo obrigatório"),
+    // .matches(
+    //   "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$",
+    //   "essa senha não é segura"
+    // ),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmitFunction = (data) => {
+    setCard(data);
+  };
+
+  console.log(errors);
+  return (
+    <div className="container">
+      <h1>sign up</h1>
+      <form className="forms" onSubmit={handleSubmit(onSubmitFunction)}>
+        <input placeholder="Nome de usuário*" {...register("user")} />
+        {errors.user && errors.user.message}
+        <input placeholder="Nome completo*" {...register("name")} />
+        {errors.name?.message}
+        <input placeholder="Endereço de email*" {...register("email")} />
+        {errors.email && errors.email.message}
+        <input placeholder="Confirme seu email*" {...register("email")} />
+        {errors.email?.message}
+        <input
+          placeholder="Contato:(81)99999-9999*"
+          {...register("cellphone")}
+        />
+        {errors.cellphone?.message}
+        <div className="inputSenha">
+          <input
+            className="senha"
+            placeholder="Senha*"
+            {...register("password")}
+          />
+          {errors.password?.message}
+          <input
+            id="input1"
+            placeholder="Confirme sua senha*"
+            {...register("passwordConfirmed")}
+          />
+          {errors.passwordConfirmed?.message}
+        </div>
+        <button>Enviar</button>
+      </form>
+      {card && <CardUser card={card} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/cardUser/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+const CardUser = ({ card }) => {
+  return (
+    <div className="infoUser">
+      <h3>Seus dados:</h3>
+      <p>Usuário: {card.user}</p>
+      <p>Nome: {card.name}</p>
+      <p>Email: {card.email}</p>
+      <p>Contato: {card.cellphone}</p>
+    </div>
+  );
+};
+export default CardUser;
